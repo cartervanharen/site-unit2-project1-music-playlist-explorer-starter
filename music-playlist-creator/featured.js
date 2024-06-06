d9c251fdee3622d4cfddf954b3e109ea98b60a21
Carter Min-Kee Vanharen <cartervanharen@meta.com>
2024-06-06 15:18:27 -0700

    final done

--- music-playlist-creator/featured.js
+++ music-playlist-creator/featured.js
@@ -13,12 +13,12 @@ window.onload = function () {
         const displayDiv = document.getElementById("featuredstuff");
       
         displayDiv.innerHTML = `
-          <div class="featbody" id="topmodal">
+          <div style="padding=30px" class="featbody" id="topmodal">
             <div style="display: inline-flex; align-items: center;">
               <img
                 id="imgplayinmodel"
                 src="${data.playlists[playlistID].playlist_art}"
-                style="  opacity: 100%;  width: 180px; height: 180px; border-radius: 10px; margin-right: auto; margin-bottom: 10px;"
+                style="  opacity: 100%;  width: 300px; height: 300px; border-radius: 10px; margin-right: 30px; margin-bottom: 10px;"
               />
               <div style="padding-left: 10px;">
                 <h1 style="font-size: 22pt">${data.playlists[playlistID].playlist_name}</h1>
@@ -35,13 +35,72 @@ window.onload = function () {
         `;
       
      
-  
 
+  
+        fillsongs(playlistID)
 }
 
 
 
 
+
+function fillsongs(playlistId) {
+    const playlist = data.playlists.find(
+      (pl) => pl.playlistID === parseInt(playlistId)
+    );
+  
+    const displayDiv = document.getElementById("showsongshere");
+  
+    playlist.songs.forEach((song) => {
+      const songCard = `
+        <li style="padding-top: 10px;" class="songitemcard2">
+          <div
+            style="
+              flex: 0 0 auto;
+              margin-right: 20px;
+              border-radius: 10px;
+            "
+          >
+            <img
+              src="${song.cover_art}"
+              style="
+                border-radius: 10px;
+                width: 120px;
+                height: 120px;
+                margin-left: 10px;
+              "
+            />
+          </div>
+          
+          <div style="flex: 1; text-align: left">
+            <h1>${song.title}</h1>
+            <p>${song.artist}</p>
+            <p>${song.album}</p>
+          </div>
+          
+          <div
+            style="
+              flex: 0 0 auto;
+              text-align: right;
+              align-self: center;
+            "
+          >
+            <p>${song.duration}</p>
+          </div>
+        </li>`;
+  
+      displayDiv.innerHTML += songCard;
+    });
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 
 
